1cfae0baabfc05c4e474530cba652f29f0951989
Price Tseng <Everett6802@hotmail.com>
2016-03-04 14:19:27 +0800

    Remove some useless codes

--- finance_analyzer_test.cpp
+++ finance_analyzer_test.cpp
@@ -379,13 +379,6 @@ void FinanceAnalyzerTest::test_check_filter_array()
 	sp_long_data_array->set_type(FinanceField_LONG);
 	sp_float_data_array->set_type(FinanceField_FLOAT);
 
-// Check Avg/Diff array
-//	if (show_test_case_detail) cout << "2 Diff: " << *result_set.get_array(FinanceSource_StockExchangeAndVolume, 2, ArrayElementCalculation_Diff) << endl;
-//	if (show_test_case_detail) cout << "2 Avg5: " << *result_set.get_array(FinanceSource_StockExchangeAndVolume, 2, ArrayElementCalculation_Avg5) << endl;
-//	if (show_test_case_detail) cout << "2 Avg10: " << *result_set.get_array(FinanceSource_StockExchangeAndVolume, 2, ArrayElementCalculation_Avg10) << endl;
-//	if (show_test_case_detail) cout << "4 Diff: " << *result_set.get_array(FinanceSource_StockExchangeAndVolume, 4, ArrayElementCalculation_Diff) << endl;
-//	if (show_test_case_detail) cout << "4 Avg5: " << *result_set.get_array(FinanceSource_StockExchangeAndVolume, 4, ArrayElementCalculation_Avg5) << endl;
-
 	PFINANCE_LONG_DATA_ARRAY data_array1 = (PFINANCE_LONG_DATA_ARRAY)result_set.get_array(FinanceSource_StockExchangeAndVolume, 1);
 	ret = data_array1->get_sub_array(*sp_long_data_array, &filter_data_array, 0);
 	if (CHECK_FAILURE(ret))
@@ -401,80 +394,6 @@ void FinanceAnalyzerTest::test_check_filter_array()
 		throw runtime_error(string(errmsg));
 	}
 	sp_long_data_array->reset_array();
-//	ret = data_array1->get_diff_array(*sp_long_data_array, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [1 Diff], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "1 Diff: " << *sp_long_data_array << endl;
-//	static const long ARRAY1_DIFF[] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
-//	if (*sp_long_data_array != ARRAY1_DIFF)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[1 Diff] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_long_data_array->reset_array();
-//
-//	ret = data_array1->get_sum_array(*sp_long_data_array, 5, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [1 Sum5], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "1 Sum5: " << *sp_long_data_array << endl;
-//	static const long ARRAY1_SUM5[] = {15, 20, 25, 30, 35, 40};
-//	if (*sp_long_data_array != ARRAY1_SUM5)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[1 Sum5] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_long_data_array->reset_array();
-//
-//	ret = data_array1->get_avg_array(*sp_float_data_array, 5, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [1 Avg5], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "1 Avg5: " << *sp_float_data_array << endl;
-//	static const float ARRAY1_AVG5[] = {3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
-//	if (*sp_float_data_array != ARRAY1_AVG5)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[1 Avg5] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
-//
-//	ret = data_array1->get_weighted_avg_array(*sp_float_data_array, 5, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [1 WAvg5], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "1 WAvg5: " << *sp_float_data_array << endl;
-//	static const float ARRAY1_WAVG5[] = {3.56, 4.56, 5.56, 6.56, 7.56, 8.56};
-//	if (*sp_float_data_array != ARRAY1_WAVG5)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[1 WAvg5] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
-//
-//	ret = data_array1->get_weighted_avg_array(*sp_float_data_array, 10, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [1 WAvg10], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "1 WAvg10: " << *sp_float_data_array << endl;
-//	static const float ARRAY1_WAVG10[] = {6.61};
-//	if (*sp_float_data_array != ARRAY1_WAVG10)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[1 WAvg10] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	PFINANCE_INT_DATA_ARRAY data_array3 = (PFINANCE_INT_DATA_ARRAY)result_set.get_array(FinanceSource_StockExchangeAndVolume, 3);
 	ret = data_array3->get_sub_array(*sp_int_data_array, &filter_data_array, 0);
@@ -491,80 +410,6 @@ void FinanceAnalyzerTest::test_check_filter_array()
 		throw runtime_error(string(errmsg));
 	}
 	sp_int_data_array->reset_array();
-//	ret = data_array3->get_diff_array(*sp_int_data_array, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [3 Diff], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "3 Diff: " << *sp_int_data_array << endl;
-//	static const int ARRAY3_DIFF[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
-//	if (*sp_int_data_array != ARRAY3_DIFF)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[3 Diff] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_int_data_array->reset_array();
-//
-//	ret = data_array3->get_sum_array(*sp_int_data_array, 5, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [3 Sum5], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "3 Sum5: " << *sp_int_data_array << endl;
-//	static const int ARRAY3_SUM5[] = {-15, -20, -25, -30, -35, -40};
-//	if (*sp_int_data_array != ARRAY3_SUM5)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[3 Sum5] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_int_data_array->reset_array();
-//
-//	ret = data_array3->get_avg_array(*sp_float_data_array, 5, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [3 Avg5], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "3 Avg5: " << *sp_float_data_array << endl;
-//	static const float ARRAY3_AVG5[] = {-3.0, -4.0, -5.0, -6.0, -7.0, -8.0};
-//	if (*sp_float_data_array != ARRAY3_AVG5)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[3 Avg5] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
-//
-//	ret = data_array3->get_weighted_avg_array(*sp_float_data_array, 5, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [3 WAvg5], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "3 WAvg5: " << *sp_float_data_array << endl;
-//	static const float ARRAY3_WAVG5[] = {-3.56, -4.56, -5.56, -6.56, -7.56, -8.56};
-//	if (*sp_float_data_array != ARRAY3_WAVG5)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[3 WAvg5] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
-//
-//	ret = data_array3->get_weighted_avg_array(*sp_float_data_array, 10, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [3 WAvg10], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "3 WAvg10: " << *sp_float_data_array << endl;
-//	static const float ARRAY3_WAVG10[] = {-6.11};
-//	if (*sp_float_data_array != ARRAY3_WAVG10)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[3 WAvg10] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	PFINANCE_FLOAT_DATA_ARRAY data_array4 = (PFINANCE_FLOAT_DATA_ARRAY)result_set.get_array(FinanceSource_StockExchangeAndVolume, 4);
 	ret = data_array4->get_sub_array(*sp_float_data_array, &filter_data_array, 0);
@@ -581,80 +426,6 @@ void FinanceAnalyzerTest::test_check_filter_array()
 		throw runtime_error(string(errmsg));
 	}
 	sp_float_data_array->reset_array();
-//	ret = data_array4->get_diff_array(*sp_float_data_array, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [4 Sum5], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "4 Diff: " << *sp_float_data_array << endl;
-//	static const float ARRAY4_DIFF[] = {10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0};
-//	if (*sp_float_data_array != ARRAY4_DIFF)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[4 Diff] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
-//
-//	ret = data_array4->get_sum_array(*sp_float_data_array, 5, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [4 Sum5], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "4 Sum5: " << *sp_float_data_array << endl;
-//	static const float ARRAY4_SUM5[] = {150.5, 200.5, 250.5, 300.5, 350.5, 400.5};
-//	if (*sp_float_data_array != ARRAY4_SUM5)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[4 Sum5] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
-//
-//	ret = data_array4->get_avg_array(*sp_float_data_array, 5, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [4 Avg5], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "4 Avg5: " << *sp_float_data_array << endl;
-//	static const float ARRAY4_AVG5[] = {30.1, 40.1, 50.1, 60.1, 70.1, 80.1};
-//	if (*sp_float_data_array != ARRAY4_AVG5)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[4 Avg5] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
-//
-//	ret = data_array4->get_weighted_avg_array(*sp_float_data_array, 5, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [4 WAvg5], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "4 WAvg5: " << *sp_float_data_array << endl;
-//	static const float ARRAY4_WAVG5[] = {35.66, 45.66, 55.66, 65.66, 75.66, 85.66};
-//	if (*sp_float_data_array != ARRAY4_WAVG5)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[4 WAvg5] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
-//
-//	ret = data_array4->get_weighted_avg_array(*sp_float_data_array, 10, 0);
-//	if (CHECK_FAILURE(ret))
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "Fail to get Array [4 WAvg10], due to: %s", get_ret_description(ret));
-//		throw runtime_error(string(errmsg));
-//	}
-//	if (show_test_case_detail) cout << "4 WAvg10: " << *sp_float_data_array << endl;
-//	static const float ARRAY4_WAVG10[] = {66.21};
-//	if (*sp_float_data_array != ARRAY4_WAVG10)
-//	{
-//		snprintf(errmsg, ERRMSG_SIZE, "The Array[4 WAvg10] is NOT correct");
-//		throw runtime_error(string(errmsg));
-//	}
-//	sp_float_data_array->reset_array();
 }
 
 void FinanceAnalyzerTest::test_check_formula()
